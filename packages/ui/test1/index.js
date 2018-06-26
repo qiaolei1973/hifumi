8f67f619434e47bfa993185ed28c6a9c91a70684
qiaolei@gagogroup.com <qiaolei1973@gmail.com>
2018-06-26 12:02:23 +0800

    chore(config): add build-lib config by tsc

---
+++ packages/ui/test1/index.js
@@ -0,0 +1,6 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", { value: true });
+var Matrix_1 = require("./Matrix");
+exports.Matrix = Matrix_1.default;
+//# sourceMappingURL=index.jsx.map
