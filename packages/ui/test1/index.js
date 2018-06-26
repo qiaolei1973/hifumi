599ef9d6083ef8a3baa9c37d1e9f5eb48294a366
qiaolei@gagogroup.com <qiaolei1973@gmail.com>
2018-06-26 22:14:16 +0800

    chore(location): remove useless file

--- packages/ui/test1/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", { value: true });
-var Matrix_1 = require("./Matrix");
-exports.Matrix = Matrix_1.default;
-//# sourceMappingURL=index.jsx.map
